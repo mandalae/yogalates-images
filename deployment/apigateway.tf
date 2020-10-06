8c70abe943de5e0adfdfb5541f740c7a96b1eb92
Chris Skaaning <c@rpediem.com>
2020-10-06 23:50:20 +0100

    Adding API gateway for images

---
+++ deployment/apigateway.tf
@@ -0,0 +1,42 @@
+variable "rest_api_id" {
+    default = "ul55ggh6oa"
+}
+
+resource "aws_api_gateway_resource" "Yogalates-images" {
+  rest_api_id = var.rest_api_id.value
+  parent_id   = "7718cgl77a"
+  path_part   = "images"
+}
+
+resource "aws_api_gateway_method" "GET_images" {
+  rest_api_id   = var.rest_api_id.value
+  resource_id   = aws_api_gateway_resource.Yogalates-images.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = "dsc7wd"
+
+  request_parameters = {
+    "method.request.header.X-Authorization" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "GET_images_integration" {
+  depends_on              = [aws_api_gateway_method.GET_images]
+  rest_api_id             = var.rest_api_id.value
+  resource_id             = aws_api_gateway_resource.Yogalates-images.id
+  http_method             = aws_api_gateway_method.GET_images.http_method
+  integration_http_method = aws_api_gateway_method.GET_images.http_method
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.Yogalates-images.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "Yogalates-API-deployment" {
+  depends_on = [aws_api_gateway_integration.GET_images_integration]
+
+  rest_api_id = var.rest_api_id.value
+  stage_name  = "prod"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
