ba33e5195eb04754e17bf0d87d9dea8aef10313a
Chris Skaaning <c@rpediem.com>
2020-10-06 23:54:45 +0100

    Adding API gateway for images

--- deployment/lambda.tf
+++ deployment/lambda.tf
@@ -6,6 +6,8 @@ resource "aws_lambda_permission" "Yogalates-images" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.Yogalates-images.function_name}"
   principal     = "apigateway.amazonaws.com"
+
+  source_arn = "arn:aws:execute-api:eu-west-1:368263227121:${var.rest_api_id.value}/*/${aws_api_gateway_method.GET_images.http_method}${aws_api_gateway_resource.Yogalates-images.path}"
 }
 
 resource "aws_lambda_function" "Yogalates-images" {
@@ -15,6 +17,4 @@ resource "aws_lambda_function" "Yogalates-images" {
   handler       = "index.handler"
   source_code_hash = "${filebase64sha256("../artifact/artifact.zip")}"
   runtime       = "nodejs12.x"
-  
-  source_arn = "arn:aws:execute-api:eu-west-1:368263227121:${var.rest_api_id.value}/*/${aws_api_gateway_method.GET_images.http_method}${aws_api_gateway_resource.Yogalates-images.path}"
 }
