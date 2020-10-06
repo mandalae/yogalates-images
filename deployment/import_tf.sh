17e1ecf521fe8e41a2ae215561a8b28d8101af95
Chris Skaaning <c@rpediem.com>
2020-10-06 22:44:54 +0100

    Import s3 bucket

--- deployment/import_tf.sh
+++ deployment/import_tf.sh
@@ -5,7 +5,7 @@ rm plan.tf || true
 ../terraform import aws_lambda_function.Yogalates-images Yogalates-images || true
 
 ../terraform import module.cdn.aws_cloudfront_origin_access_identity.default[0] EIMIIXLMVEYV || true
-../terraform import module.cdn.module.logs.aws_s3_bucket.default[0] yogalates-prod-yogalatescdn-origin || true
+../terraform import module.cdn.module.logs.aws_s3_bucket.default[0] yogalates-prod-yogalatescdn-logs || true
 ../terraform import module.cdn.aws_s3_bucket_policy.default[0] yogalates-prod-yogalatescdn-logs || true
 ../terraform import module.cdn.module.logs.aws_s3_bucket_public_access_block.default[0] yogalates-prod-yogalatescdn-logs || true
 ../terraform import module.cdn.aws_s3_bucket.origin[0] yogalates-prod-yogalatescdn-origin
