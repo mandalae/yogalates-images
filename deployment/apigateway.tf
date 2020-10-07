6c08d4828c67f38081e3cbc196e7255e8a2d2386
Chris Skaaning <c@rpediem.com>
2020-10-07 10:59:28 +0100

    Only set up the images method instead of the resource as well

--- deployment/apigateway.tf
+++ deployment/apigateway.tf
@@ -2,9 +2,13 @@ variable "rest_api_id" {
     default = "ul55ggh6oa"
 }
 
+variable "images_resource_id" {
+    default = "rtiutq"
+}
+
 resource "aws_api_gateway_method" "GET_images" {
   rest_api_id   = var.rest_api_id
-  resource_id   = "rtiutq"
+  resource_id   = var.images_resource_id
   http_method   = "GET"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = "dsc7wd"
@@ -17,7 +21,7 @@ resource "aws_api_gateway_method" "GET_images" {
 resource "aws_api_gateway_integration" "GET_images_integration" {
   depends_on              = [aws_api_gateway_method.GET_images]
   rest_api_id             = var.rest_api_id
-  resource_id             = aws_api_gateway_resource.Yogalates-images.id
+  resource_id             = var.images_resource_id
   http_method             = aws_api_gateway_method.GET_images.http_method
   integration_http_method = aws_api_gateway_method.GET_images.http_method
   type                    = "AWS_PROXY"
